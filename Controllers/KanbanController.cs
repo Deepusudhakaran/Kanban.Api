9d2649239532421f39cd0864ce0608c9d1654cf8
Deepu Sudhakaran <deepusudh@gmail.com>
2026-05-23 21:59:28 +1000

    Initial API structure

---
+++ Controllers/KanbanController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kanban.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class KanbanController() : ControllerBase
+    {
+
+    }
+}
